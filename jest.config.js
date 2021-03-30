adbd9c9eabf476ab849c75639624722b6d4f514d
SabrinaTravasso <53414857+SabrinaTravasso@users.noreply.github.com>
2021-03-30 16:28:23 -0300

    Initial commit from Create Next App

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx']
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/lontra.jpg" />
+        <link rel="apple-touch-icon" href="/img/lontra.jpg" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
